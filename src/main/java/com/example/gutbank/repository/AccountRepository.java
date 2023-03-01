25bec0aa464c43c1eb5de90c7060d165dc53dd1d
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-03-01 19:03:20 +0100

    Start_Structure_1.13

--- src/main/java/com/example/gutbank/repository/AccountRepository.java
+++ src/main/java/com/example/gutbank/repository/AccountRepository.java
@@ -3,9 +3,13 @@ package com.example.gutbank.repository;
 import com.example.gutbank.entity.Account;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface AccountRepository extends CrudRepository<Account, Integer> {
+    List<Account> findById(String id);
 
+    Account findByIdAndName(String id, String name);
 }
--- src/main/java/com/example/gutbank/repository/ClientRepository.java
+++ src/main/java/com/example/gutbank/repository/ClientRepository.java
@@ -1,11 +1,17 @@
 package com.example.gutbank.repository;
 
+import java.util.List;
+
 import com.example.gutbank.entity.Client;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
 
-public interface ClientRepository extends CrudRepository<Client, Integer> {
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface ClientRepository extends PagingAndSortingRepository<Client, Long>, CrudRepository<Client, Long> {
+
+    List<Client> findByLastName(@Param("last_name") String lastName);
 
 }
