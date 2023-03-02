5178387a9ad22baec86699196df8b2df86cf8ba8
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-03-02 22:09:55 +0100

    Start_Structure_1.18Success

--- src/main/java/com/example/gutbank/repository/ClientRepository.java
+++ src/main/java/com/example/gutbank/repository/ClientRepository.java
@@ -6,10 +6,10 @@ import com.example.gutbank.entity.Client;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+//@RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface ClientRepository extends PagingAndSortingRepository<Client, Long>, CrudRepository<Client, Long> {
 
     List<Client> findByLastName(@Param("last_name") String lastName);
