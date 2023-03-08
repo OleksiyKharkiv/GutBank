2e4788d7d414f0d75f37bdcba32e548406fec90b
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-03-08 10:49:12 +0100

    Start_Structure_1.2.Success. Removed secret data to environment variables.

--- src/main/java/com/example/gutbank/repository/ClientRepository.java
+++ src/main/java/com/example/gutbank/repository/ClientRepository.java
@@ -1,13 +1,13 @@
 package com.example.gutbank.repository;
 
 import com.example.gutbank.entity.Client;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ClientRepository extends PagingAndSortingRepository<Client, Long>, CrudRepository<Client, Long> {
+public interface ClientRepository extends PagingAndSortingRepository<Client, Long>, JpaRepository<Client, Long> {
 
     List<Client> findByLastName(@Param("last_name") String lastName);
 
