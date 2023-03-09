c900c811df6e6af46c925d8b5d61851068e751a8
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-03-09 11:43:01 +0100

    Start_Structure_1.3.Success. Final. Code cleaned. Ready for PullRequest

--- src/main/java/com/example/gutbank/repository/AccountRepository.java
+++ src/main/java/com/example/gutbank/repository/AccountRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-
 public interface AccountRepository extends JpaRepository<Account, Integer> {
     List<Account> findById(String id);
 
