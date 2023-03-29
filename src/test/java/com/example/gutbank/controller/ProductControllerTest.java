a0add57e6bce45d7bd1762ededdffec4679264d9
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-03-29 20:22:01 +0200

    BAN17.7.Controller Product (findAllChangedProducts) All ok.

--- src/test/java/com/example/gutbank/controller/ProductControllerTest.java
+++ src/test/java/com/example/gutbank/controller/ProductControllerTest.java
@@ -10,8 +10,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +19,8 @@ import java.util.List;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 @AutoConfigureMockMvc
 @WebMvcTest(ProductController.class)
@@ -39,18 +35,20 @@ public class ProductControllerTest {
     void productDtoList() throws Exception {
         final List<ProductDto> productDtoList = new ArrayList<>();
         final ProductDto productDto = DtoCreator.getProductDto();
+        productDtoList.add(productDto);
+
         when(productService.getFindAllChangedProducts()).thenReturn(productDtoList);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/product/all-changed"))
                 .andExpect(status().isOk())
                 .andDo(print())
-                .andExpect((ResultMatcher) jsonPath("$", hasSize(1)))
-                .andExpect((ResultMatcher) jsonPath("$[0].id").value(productDto.getId()))
-                .andExpect((ResultMatcher) jsonPath("$[0].name").value(productDto.getName()))
-                .andExpect((ResultMatcher) jsonPath("$[0].status", is(productDto.getStatus())))
-                .andExpect((ResultMatcher) jsonPath("$[0].currency", is(productDto.getCurrency())))
-                .andExpect((ResultMatcher) jsonPath("$[0].interestRate", is(productDto.getInterestRate())))
-                .andExpect((ResultMatcher) jsonPath("$[0].createdAt", is(productDto.getCreatedAt())))
-                .andExpect((ResultMatcher) jsonPath("$[0].updatedAt", is(productDto.getUpdatedAt())));
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", is(productDto.getId())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name", is(productDto.getName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].status", is(productDto.getStatus().toString())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].currency", is(productDto.getCurrency().toString())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].interestRate").value(productDto.getInterestRate()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].limit").value(productDto.getLimit()))
+         ;
     }
 }
